cfee7326c4b5e865cec2f277670e72ac4574ac1e
santi19999 <santiherrera1999@gmail.com>
2021-11-11 23:34:13 -0300

    Nuevo Sup.User , agregamos modelos a admin

--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Clientes(models.Model):
     nombre=models.CharField(max_length=30)
     direccion=models.CharField(max_length=50)
-    email=models.EmailField()
+    email=models.EmailField(blank=True, null=True)#Le indicamos a django que el campo no es requerido y puede quedar vacío
     tel=models.CharField(max_length=7)
 
 class Articulos(models.Model):
@@ -13,7 +13,13 @@ class Articulos(models.Model):
     seccion=models.CharField(max_length=30)
     precio=models.IntegerField()
     
+    def __str__(self): #Para poder mostrar los datos de la base de datos ya que los retorna como objetos query
+        return 'El nombre es %s, la sección es %s y el precio es %s' % (self.nombre,self.seccion,self.precio)
+    
+    
 class Pedidos(models.Model):
     numero=models.IntegerField()
     fecha=models.DateField()
     entregado=models.BooleanField()
+    
+    
