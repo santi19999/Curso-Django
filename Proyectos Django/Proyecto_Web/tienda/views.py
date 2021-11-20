7260e382fe5aa566d3e1681bbd010df6a748a73d
santi19999 <santiherrera1999@gmail.com>
2021-11-19 22:36:01 -0300

    Registro en el admin

--- Proyectos Django/Proyecto_Web/tienda/views.py
+++ Proyectos Django/Proyecto_Web/tienda/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 
 def tienda(request):
-    return render (request, "tienda/tienda.html")
+    productos=Producto.objects.all()
+    return render (request, "tienda/tienda.html",{"productos": productos})
+
+
+'''posts=Post.objects.all() #importa todos los blogs
+    return render (request, "blog/blog.html", {"posts": posts})'''
