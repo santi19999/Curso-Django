d5d7d76c59c93dd5ee0c5fb730a9ff113b54da08
santi19999 <santiherrera1999@gmail.com>
2021-11-24 23:22:30 -0300

    creamos el widget del carro

---
+++ Proyectos Django/Proyecto_Web/carro/context_processor.py
@@ -0,0 +1,11 @@
+from .carro import Carro
+def importe_total_carro(request):
+    total=125
+    miCarro=Carro(request)
+    if request.user.is_authenticated:
+        
+        for key, value in request.session["carro"].items():
+
+            total=total+(float(value['precio'])*value['cantidad'])
+
+        return {'importe_total_carro':total}
